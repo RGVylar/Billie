2e46f3ef4549effe302076d6f77dfbdf2d7137c0
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-02 15:52:53 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,11 +1,10 @@
-const ytdl = require('ytdl-core');
-const queue = new Map();
 module.exports = {
   name: 'sv_play',
   description: 'play!',
-  execute(msg, args) {
+  execute: async (msg, args) => {
+    const ytdl = require('ytdl-core');
     if (msg.member.voice.channel) {
-      const connection = msg.member.voice.channel.join();
+      const connection = awaits msg.member.voice.channel.join();
     } else {
       msg.reply('You need to join a voice channel first!');
     }
