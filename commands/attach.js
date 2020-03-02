bc05143eddea60f004081bfe203b84940f0738e7
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-02 12:32:58 +0100

    Add files via upload

--- commands/attach.js
+++ commands/attach.js
@@ -2,7 +2,15 @@ module.exports = {
   name: 'sv_attach',
   description: 'attach!',
   execute(msg, args) {
-    msg.channel.send(MessageAttachment('https://i.imgur.com/w3duR07.png'));
+    const embed = new MessageEmbed()
+      // Set the title of the field
+      .setTitle('A slick little embed')
+      // Set the color of the embed
+      .setColor(0xff0000)
+      // Set the main content of the embed
+      .setDescription('Hello, this is a slick embed!');
+    // Send the embed to the same channel as the message
+    msg.channel.send(embed);
     msg.delete();
   },
 };
