c548c81eca821b0cca346f171f5741fc215bd3e8
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-02 15:47:47 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -3,9 +3,9 @@ const queue = new Map();
 module.exports = {
   name: 'sv_play',
   description: 'play!',
-  execute( msg, args) {
+  async execute( msg, args) {
     if (msg.member.voice.channel) {
-      const connection = msg.member.voice.channel.join();
+      const connection = awaits msg.member.voice.channel.join();
     } else {
       msg.reply('You need to join a voice channel first!');
     }
