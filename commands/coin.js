e7e44b522f3e95eaa1b66ff1e44a354b0fb42480
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-02 10:31:09 +0100

    Update coin.js

--- commands/coin.js
+++ commands/coin.js
@@ -5,7 +5,7 @@ module.exports = {
     var flipcoin = ["heads", "tails"];
     const member = msg.mentions.members.first() || msg.guild.members.get(args[0]) || msg.member;
     var randomIndex = Math.floor(Math.random() * flipcoin.length); 
-    msg.channel.send(flipcoin[randomIndex]);
+    msg.reply(', ' + flipcoin[randomIndex]);
   },
 };
 
