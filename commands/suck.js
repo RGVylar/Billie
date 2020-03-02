bf729287d03d4e15ee33033a1505f1b722e70410
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-02 10:32:25 +0100

    Update suck.js

--- commands/suck.js
+++ commands/suck.js
@@ -2,6 +2,6 @@ module.exports = {
   name: 'sv_suck',
   description: 'Suck my dick!',
   execute(msg, args) {
-    msg.channel.send('Why are ypu trying this?');
+    msg.channel.send('Why are you trying this?');
   },
 };
