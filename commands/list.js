8a919c7fda59a36de91b3d4c5f8ae89facf20055
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-02 10:35:17 +0100

    Update list.js

--- commands/list.js
+++ commands/list.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'sv_list',
   description: 'list!',
   execute(msg, args) {
-    msg.channel.send('```fix\nCommand list =\nsv_avatar: sends avatar link\nsv_coin: flips a coin\nsv_dice: rolls a dice\nsv_join: joins channel\nsv_lolito: super secret command\nsv_ping: test command\nsv_play: this is not working yet\nsv_sa: I miss this command\nsv_stop: get out of this channel\nsv_suck: n-nani?...\nsv_turn: Tekkus command\n```');
+    msg.channel.send('```fix\nCommand list =\nsv_avatar: sends avatar link\nsv_coin: flips a coin\nsv_dice: rolls a dice\nsv_join: joins channel\nsv_lolito: super secret command\nsv_ping: test command\nsv_play: this is not working yet\nsa: I miss this command\nsv_stop: get out of this channel\nsv_suck: n-nani?...\nsv_turn: Tekkus command\n```');
     msg.delete();
   },
 };
