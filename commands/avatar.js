2d007cfe0393137f34fb9ab1d0cd6fa1b1a9bdb6
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-02 21:55:07 +0100

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -7,7 +7,7 @@ module.exports = {
 		console.log(msg.author.displayAvatarURL);
 		const exampleEmbed = new Discord.RichEmbed()
 		.setColor('#0099ff')
-		.setTitle('${msg.author.username}')
+		.setTitle(msg.author.username`'s pfp`)
 		.setImage(msg.author.displayAvatarURL)
 		.setTimestamp();
 		//msg.channel.send(`Your avatar: <${msg.author.displayAvatarURL}>`);
