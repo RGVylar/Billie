920a4279dbe82b5705bee5e093c25f42764ccdf7
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-02 13:06:08 +0100

    Update attach.js

--- commands/attach.js
+++ commands/attach.js
@@ -1,17 +1,24 @@
-const MessageEmbed = require('discord.js');
 module.exports = {
   name: 'sv_attach',
   description: 'attach!',
   execute(msg, args) {
-    const embed = new MessageEmbed()
-      // Set the title of the field
-      .setTitle('A slick little embed')
-      // Set the color of the embed
-      .setColor(0xff0000)
-      // Set the main content of the embed
-      .setDescription('Hello, this is a slick embed!');
-    // Send the embed to the same channel as the message
-    msg.channel.send(embed);
+    const exampleEmbed = new Discord.RichEmbed()
+	.setColor('#0099ff')
+	.setTitle('Some title')
+	.setURL('https://discord.js.org/')
+	.setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+	.setDescription('Some description here')
+	.setThumbnail('https://i.imgur.com/wSTFkRM.png')
+	.addField('Regular field title', 'Some value here')
+	.addBlankField()
+	.addField('Inline field title', 'Some value here', true)
+	.addField('Inline field title', 'Some value here', true)
+	.addField('Inline field title', 'Some value here', true)
+	.setImage('https://i.imgur.com/wSTFkRM.png')
+	.setTimestamp()
+	.setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+
+msg.channel.send(exampleEmbed);
     msg.delete();
   },
 };
