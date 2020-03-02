533cac91dd5c4d91e808cb8defab4fc91bc31e51
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-02 15:22:09 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -4,10 +4,11 @@ module.exports = {
   name: 'sv_play',
   description: 'play!',
   execute(msg, args) {
-    const ytdl = require('ytdl-core');
-     const voiceChannel = msg.member.voiceChannel;
-        if (!voiceChannel || voiceChannel.type !== 'voice') return msg.reply('I couldn\'t connect to your voice channel...');
-       const connection = voiceChannel.join();
+    if (msg.member.voice.channel) {
+      const connection = await msg.member.voice.channel.join();
+    } else {
+      msg.reply('You need to join a voice channel first!');
+    }
     connection.play(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { filter: 'audioonly' }));
   },
 };
