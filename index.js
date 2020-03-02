47b4d21c7c1d1aac928780ea4a193763ef6c0433
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-02 12:10:04 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 require('dotenv').config();
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-const { MessageAttachment } = require('discord.js');
 bot.commands = new Discord.Collection();
 const botCommands = require('./commands');
 
