5d5710f017c3af78a1f43309187e0cc3c597bd0f
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-02 13:36:21 +0100

    Update dance.js

--- commands/dance.js
+++ commands/dance.js
@@ -1,10 +1,17 @@
+const Discord = require('discord.js');
 module.exports = {
   name: 'sv_dance',
   description: 'dance!',
   execute(msg, args) {
-    //msg.channel.send("Testing message.", { files: ["https://media.discordapp.net/attachments/652432414135681060/680578706875482132/tenor_2.gif"] });
-    //.channel.send('<https://media.discordapp.net/attachments/652432414135681060/680578706875482132/tenor_2.gif>');
-    msg.channel.send({ files: ["https://media.discordapp.net/attachments/652432414135681060/680578706875482132/tenor_2.gif"] });
+    
+    const exampleEmbed = new Discord.RichEmbed()
+	.setColor('#0099ff')
+	.setTitle('Lets dance!')
+	.setImage('https://media.discordapp.net/attachments/652432414135681060/680578706875482132/tenor_2.gif')
+	.setTimestamp();
+
+msg.channel.send(exampleEmbed);
+    //msg.channel.send({ files: ["https://media.discordapp.net/attachments/652432414135681060/680578706875482132/tenor_2.gif"] });
     msg.delete();
   },
 };
