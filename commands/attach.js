4e4064f541596dfda47e55ad41028171b67421f3
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-02 12:26:07 +0100

    Add files via upload

--- commands/attach.js
+++ commands/attach.js
@@ -1,11 +1,8 @@
-const { Client, MessageAttachment } = require('discord.js');
 module.exports = {
   name: 'sv_attach',
   description: 'attach!',
   execute(msg, args) {
-    const attachment = new MessageAttachment('https://i.imgur.com/w3duR07.png');
-    // Send the attachment in the message channel
-    msg.channel.send(attachment);
+    msg.channel.send(MessageAttachment('https://i.imgur.com/w3duR07.png'));
     msg.delete();
   },
 };
