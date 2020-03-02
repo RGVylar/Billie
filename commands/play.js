21fe6ac295336c701a11ad55887557662fff48ea
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-02 16:42:24 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -4,10 +4,17 @@ module.exports = {
   execute: async (msg, args) => {
     const ytdl = require('ytdl-core');
     if (msg.member.voice.channel) {
-      const connection = await msg.member.voice.channel.join();
+      const connection = await msg.member.voice.channel.join().then(connection => {
+            console.log("joined channel");
+            const stream = ytdl('https://youtu.be/nGfS3y3wnfc', { filter : 'audioonly' });
+            const dispatcher = connection.playStream(stream, streamOptions);
+            dispatcher.on("end", end => {
+                console.log("left channel");
+                voiceChannel.leave();
+            });
+        }).catch(err => console.log(err));
     } else {
       msg.reply('You need to join a voice channel first!');
     }
-    connection.play(ytdl('https://youtu.be/nGfS3y3wnfc', { filter: 'audioonly' }));
   },
 };
