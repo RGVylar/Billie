c35c4a81fec236ee11796c7d3a0fc92c044cd27f
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-02 21:45:37 +0100

    Update avatar.js

--- commands/avatar.js
+++ commands/avatar.js
@@ -3,13 +3,13 @@ module.exports = {
   description: 'avatar!',
   execute(msg, args) {
   	if (!msg.mentions.users.size) {
-		return const exampleEmbed = new Discord.RichEmbed()
+		const exampleEmbed = new Discord.RichEmbed()
 		.setColor('#0099ff')
 		.setTitle('Lets dance!')
 		.setImage('${msg.author.displayAvatarURL}')
 		.setTimestamp();
 		//msg.channel.send(`Your avatar: <${msg.author.displayAvatarURL}>`);
-		msg.channel.send(exampleEmbed);
+		return msg.channel.send(exampleEmbed);
 	}
 	const avatarList = msg.mentions.users.map(user => {
 		return `${user.username}'s avatar: <${user.displayAvatarURL}>`;
