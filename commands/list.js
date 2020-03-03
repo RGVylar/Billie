417642dfb497d5458777922ab6f69cd87ad0e024
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-03 10:19:14 +0100

    Update list.js

--- commands/list.js
+++ commands/list.js
@@ -5,7 +5,7 @@ module.exports = {
   execute(msg, args) {
     var files = fs.readdirSync('./.');
     console.log(files.length);
-    msg.channel.send('```fix\nCommand list =\navatar: sends avatar link\ncoin: flips a coin\ndice: rolls a dice\njoin: joins channel\nlolito: super secret command\nping: test command\nplay: this is not working yet\nsa: I miss this command\nstop: get out of this channel\nsuck: n-nani?...\nturn: Tekkus command\n```');
+    msg.channel.send(files.join("\n"));
     msg.delete();
   },
 };
