8d7649f07730759b735ea7ef53c63873518c3ea8
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-03 16:54:33 +0100

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -13,8 +13,12 @@ module.exports = {
 		const b = userb.indexOf("#");
 		const  resa = usera.substring(0, a);
 		const  resb = userb.substring(0, b);
-    var gifs = ["https://images-ext-1.discordapp.net/external/QYnzqF83OZ14RzVoM1t3EUw62M5x9WiIRTdwwNkVW0c/https/cdn.weeb.sh/images/HkfgF_QvW.gif"
-		 ];
+    var gifs = ["https://images-ext-1.discordapp.net/external/QYnzqF83OZ14RzVoM1t3EUw62M5x9WiIRTdwwNkVW0c/https/cdn.weeb.sh/images/HkfgF_QvW.gif",
+		"https://i.pinimg.com/originals/08/de/7a/08de7ad3dcac4e10d27b2c203841a99f.gif",
+		"https://media1.tenor.com/images/06658461277f7565cc769d894619e533/tenor.gif?itemid=14839202",
+		"https://i.imgur.com/tuH4gqZ.gif",
+		"https://pa1.narvii.com/5964/1760621601609ab2564a5bde2cc9cae0aa6ee81f_00.gif"
+	       ];
     var randomIndex = Math.floor(Math.random() * gifs.length); 
     const exampleEmbed = new Discord.RichEmbed()
 	.setColor('#0099ff')
