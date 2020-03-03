b6f69ec5a4e8e797db7d888363df92b1f3dc3de0
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-03 09:47:29 +0100

    Update lolito.js

--- commands/lolito.js
+++ commands/lolito.js
@@ -1,5 +1,5 @@
 module.exports = {
-  name: 'sv_lolito',
+  name: 'lolito',
   description: 'sa!',
   execute(msg, args) {
     msg.channel.send('<:omglolito0:679961156554850316><:omglolito1:679961519471067148><:omglolito2:679961357570801716><:omglolito3:679961357566738592>');
