44947e4a83fa6bb67d8483d0e793e8519690e876
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-03 09:06:22 +0100

    Update dance.js

--- commands/dance.js
+++ commands/dance.js
@@ -3,11 +3,12 @@ module.exports = {
   name: 'sv_dance',
   description: 'dance!',
   execute(msg, args) {
-    
+    var dances = ["https://media.discordapp.net/attachments/652432414135681060/680578706875482132/tenor_2.gif", "https://external-preview.redd.it/abIpQURl4wtDa6Y31c4RWKL7hwhgxDagpzGwFbUwMlQ.gif?s=1a81ebfe75fbadb488e6edc53e7f5d4f0b585142"];
+    var randomIndex = Math.floor(Math.random() * flipcoin.length); 
     const exampleEmbed = new Discord.RichEmbed()
 	.setColor('#0099ff')
 	.setTitle('Lets dance!')
-	.setImage('https://media.discordapp.net/attachments/652432414135681060/680578706875482132/tenor_2.gif')
+	.setImage(dance[randomIndex])
 	.setTimestamp();
 
 msg.channel.send(exampleEmbed);
