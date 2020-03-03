60786b3dcb2d93283d6e12adaba8e5b1622a9c0b
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-03 09:46:04 +0100

    Update dance.js

--- commands/dance.js
+++ commands/dance.js
@@ -1,6 +1,6 @@
 const Discord = require('discord.js');
 module.exports = {
-  name: 'sv_dance',
+  name: 'dance',
   description: 'dance!',
   execute(msg, args) {
     var dances = ["https://media.discordapp.net/attachments/652432414135681060/680578706875482132/tenor_2.gif", "https://external-preview.redd.it/abIpQURl4wtDa6Y31c4RWKL7hwhgxDagpzGwFbUwMlQ.gif?s=1a81ebfe75fbadb488e6edc53e7f5d4f0b585142"];
