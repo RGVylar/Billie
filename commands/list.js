71e929270a32d57d6ba9837c1ff62f486ec09a21
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-03 10:21:47 +0100

    Update list.js

--- commands/list.js
+++ commands/list.js
@@ -3,9 +3,9 @@ module.exports = {
   name: 'list',
   description: 'list!',
   execute(msg, args) {
-    var files = fs.readdirSync('./.');
+    var files = fs.readdirSync('./commands');
     console.log(files.length);
-    msg.channel.send(files.join("\n"));
+    msg.channel.send('```fix\nCommands =\n'+files.join("\n")+'```');
     msg.delete();
   },
 };
