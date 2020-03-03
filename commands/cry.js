15fc543e4753f1fceed513460c3725ae6ca4439c
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-03 21:35:47 +0100

    Update cry.js

--- commands/cry.js
+++ commands/cry.js
@@ -6,6 +6,8 @@ module.exports = {
     var gifs = ["https://media.tenor.com/images/8c7d0963ce62b09bd97fea22ac5e9b17/tenor.gif",
 		"https://pa1.narvii.com/6053/72c804f15039038724141a587b5dc04337dc8d9b_hq.gif",
 		"https://media1.tenor.com/images/79b965bb99fd58b94d2550b384093e75/tenor.gif?itemid=13668435",
+		"https://media1.tenor.com/images/98466bf4ae57b70548f19863ca7ea2b4/tenor.gif?itemid=14682297",
+		"https://i.pinimg.com/originals/92/d2/d0/92d2d0d6a199add0cc40ca23f39c492b.gif",
 		              "https://i.imgur.com/Y1xuiUK.gif"
 		 ];
     var randomIndex = Math.floor(Math.random() * gifs.length); 
