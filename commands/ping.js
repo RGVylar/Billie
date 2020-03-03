f68073dd7d1ed25209cf768cc12b99734e24283c
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-03 10:04:23 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -4,5 +4,6 @@ module.exports = {
   execute(msg, args) {
     msg.reply('This is a reply');
     msg.channel.send('```bash\n"This is not a reply"\n```');
+    msg.channel.sendMessage('Pong! Your ping is `' + `${Date.now() - msg.createdTimestamp}` + ' ms`');
   },
 };
