e8d2ea00310cfe14464e7e20ec263d4325b1dd26
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-04 12:33:25 +0100

    Create loli.js

---
+++ commands/loli.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+module.exports = {
+  name: 'loli',
+  description: 'loli!',
+  execute(msg, args) {
+  const user = msg.member.user.tag;
+		const n = user.indexOf("#");
+		const  res = user.substring(0, n);
+		const exampleEmbed = new Discord.RichEmbed()
+		.setColor('#0099ff')
+		.setTitle(`${res} is going to jail`)
+		.setImage('https://thumbs.gfycat.com/GiddyBrokenHog-max-1mb.gif')
+		.setTimestamp();
+		msg.channel.send(exampleEmbed);
+  },
+};
