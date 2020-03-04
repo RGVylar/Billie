47f60d5220080f6bbc4596deda8485b2ff9bfb26
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-04 15:34:40 +0100

    Update urbansfw.js

--- commands/urbansfw.js
+++ commands/urbansfw.js
@@ -21,7 +21,7 @@ module.exports = {
       .addField('Example', data.example);
     const filtercheck = ["xxx", "porn", "sex", "18+","nsfw", "hentai", "dick", "vagina", "pussy"]
     //if (filtercheck.some(word2 => data.definition.toLowerCase().includes(word2))) return msg.channel.send("Not allowed to search nsfw content.");
-    //if (filtercheck.some(word3 => data.word.toLowerCase().includes(word3))) return msg.channel.send("Not allowed to search nsfw content.");
+    if (filtercheck.some(word3 => data.word.toLowerCase().includes(word3))) return msg.channel.send("Not allowed to search nsfw content.");
     msg.channel.send(embed);
   } catch (err) {
     return msg.channel.send(`Oh no, an error occurred: \`${err.msg}\`. Try again later!`);
