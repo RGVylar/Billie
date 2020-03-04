0dd57851230e076cf9506522df1d981914d4e9f5
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-04 09:37:51 +0100

    Update translate.js

--- commands/translate.js
+++ commands/translate.js
@@ -1,11 +1,12 @@
-const translate = require('../lib/translator.js');
+const translate = require('translate');
 module.exports = {
   name: 'translate',
   description: 'translate!',
   execute(msg, args) {
   const sayMessage = args.join(" ");
-  translate.text(sayMessage, function(err, text){
-    console.log(sayMessage+' = '+text);
+    translate('Hello world', 'es').then(text => {
+    console.log(text);  // Hola mundo
+  });
     msg.channel.send(text);
     msg.delete().catch(O_o=>{}); 
  });
