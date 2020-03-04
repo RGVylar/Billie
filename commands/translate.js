552918e7bf84aac736310a06a414835380c968d2
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-04 09:16:27 +0100

    Create translate.js

---
+++ commands/translate.js
@@ -0,0 +1,14 @@
+module.exports = {
+  name: 'translate',
+  description: 'translate!',
+  execute(msg, args) {
+  const sayMessage = args.join(" ");
+  var translate = require('../lib/translator');
+ translate.text(sayMessage, function(err, text){
+ 
+    msg.delete().catch(O_o=>{}); 
+    msg.channel.send(sayMessage);
+ });
+    
+  },
+};
