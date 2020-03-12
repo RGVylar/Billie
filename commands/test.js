d07d5909f3b723c8a0e84ed189137c78f68a8e2a
Az <40351751+Mystos@users.noreply.github.com>
2020-03-12 13:58:25 +0100

    Test
    Sorry test

---
+++ commands/test.js
@@ -0,0 +1,16 @@
+const Discord = require('discord.js');
+module.exports = {
+    name: 'test',
+    description: 'AzTest',
+    execute(msg, args) {
+        //var gifs = ["
+        //];
+        var randomIndex = Math.floor(Math.random() * gifs.length);
+        const exampleEmbed = new Discord.RichEmbed()
+            .setColor('#0099ff')
+            .setTitle('Random Test');
+            //.setImage(gifs[randomIndex]);
+
+        msg.channel.send(exampleEmbed);
+    },
+};
