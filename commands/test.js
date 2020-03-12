a4623d338764d12384294d42f8be52f9b449b620
Az <40351751+Mystos@users.noreply.github.com>
2020-03-12 19:50:08 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -15,40 +15,39 @@ module.exports = {
                 .setImage("https://media1.tenor.com/images/8674cfb928b1055dd6b8227e7e61060b/tenor.gif?itemid=7979947");
 
             msg.channel.send(nsfwWrongChannelWarn);
+        } else {
+
+            // Image Feed
+            if (!args) {
+                // No tag then we take vanilla
+
+                args = vanilla;
+            }
+            var urlSearch = "https://danbooru.donmai.us/posts.json?search[tags]=" + args;
+            var postID;
+            //// Get first page post with tag
+            await fetch(urlSearch)
+                .then(response => response.json())
+                .then((data) => {
+                    var seed = Math.random() * data.length; 
+                    postID = data[seed].id;
+                })
+                .catch(err => { msg.channel.send(err) });
+
+            //Get Image
+
+
+            var urlPost = "https://danbooru.donmai.us/posts/" + postID +".json?"
+            await fetch(urlPost)
+                .then(response => response.json())
+                .then(data => { msg.channel.send("", { files: [data.file_url] }); })
+                .catch(err => { msg.channel.send(err) });
+
+            
+
+
         }
 
-        // Image Feed
-
-        var urlSearch = "https://danbooru.donmai.us/posts.json?search[tags]=" + args;
-        var list;
-
-        //// Get first page post with tag
-        //await fetch(urlSearch)
-        //    .then(response => response.json())
-        //    .then((data) => list)
-        //    .catch(err => { msg.channel.send(err) });
-
-        //msg.channel.send(list);
-
-        //var seed = Math.random() * list.length; 
-
-        //var urlPost = "https://danbooru.donmai.us/posts/" + list[seed].id +".json?"
-
-        //// Get post JSON object
-        //var postData;
-        //await fetch(urlPost)
-        //    .then(response => response.json())
-        //    .then((data) => postData)
-        //    .catch(err => { msg.channel.send(err) });
-
-        //Get Image
-
-
-
-        await fetch("https://danbooru.donmai.us/posts/3818600.json?")
-            .then(response => response.json())
-            .then(data => { msg.channel.send("", { files: [data.file_url] });})
-            .catch(err => { msg.channel.send(err) });
 
         msg.delete();
     },
