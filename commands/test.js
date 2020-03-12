b8c434efede6c9a75a5085178f61a4b977823c1f
Az <40351751+Mystos@users.noreply.github.com>
2020-03-12 16:05:36 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -2,17 +2,37 @@ const Discord = require('discord.js');
 module.exports = {
     name: 'test',
     description: 'AzTest',
-    execute(msg, args) {
+    execute:async(msg, args) {
 
+
+        // Nsfw channel test
         if (!msg.channel.nsfw) {
             const nsfwWrongChannelWarn = new Discord.RichEmbed()
-                .setColor('#0099ff')
+                .setColor('#FF0000')
                 .setTitle('You lewd !')
                 .setDescription('You need to be in a nsfw channel for that Baa~ Baka')
-                .setImage("https://tenor.com/view/lewd-gif-7979947");
+                .setImage("https://media1.tenor.com/images/8674cfb928b1055dd6b8227e7e61060b/tenor.gif?itemid=7979947");
 
             msg.channel.send(nsfwWrongChannelWarn);
         }
+
+        // Image Feed
+        const userAction = async () => {
+            const response = await fetch('https://danbooru.donmai.us/tags.json?search%5Bname_matches%5D=a*');
+            const myJson = await response.json(); //extract JSON from the http response
+            // do something with myJson
+
+            msg.channel.send(myJson);
+        }
+
+        
+
+    
+
+
+
+
+
 
         msg.delete();
     },
