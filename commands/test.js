07dd9ba2f1a931b9cf726083e8b32bf0f28efe74
Az <40351751+Mystos@users.noreply.github.com>
2020-03-12 15:07:52 +0100

    Update test.js

--- commands/test.js
+++ commands/test.js
@@ -4,26 +4,12 @@ module.exports = {
     description: 'AzTest',
     execute(msg, args) {
         
-        
+        msg.channel.send(`Args value : ${args}`);
         var prefix = 'sv_' + this.name;
         if (!msg.content.startsWith(prefix) || msg.author.bot) {
             msg.channel.send(`error you mother fockor : ${prefix}` );
             return;
         }
-
-        args = msg.content.slice(prefix.length).split(' ');
-        const command = args.shift().toLowerCase();
-        msg.channel.send(`Command value : ${command}`);
-        if (command === 'args-info') {
-            if (!args.length) {
-                return msg.channel.send(`You didn't provide any arguments, ${msg.author}!`);
-            }
-
-            msg.channel.send(`Command name: ${command}\nArguments: ${args}`);
-
-        }
-
-
         msg.delete();
     },
 };
