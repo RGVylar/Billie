bda2a62dada1ba841f8fda2d59cca605d2d2502f
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-13 18:16:58 +0100

    Update mongo.js

--- commands/mongo.js
+++ commands/mongo.js
@@ -1,8 +1,10 @@
 const mongoose = require('mongoose');
+const config = require("../config.js");
 module.exports = {
   name: 'mongo',
   description: 'mongo!',
   execute(msg, args,MONGO,mongoose) {
+	const MONGO = config.MONGO;
 	mongoose.connect(MONGO, {useNewUrlParser: true});
 	var db = mongoose.connection;
 	db.on('error', msg.channel.send("connection error"));
