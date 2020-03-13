13e69d4efe1e7a147a5d9fc88c0c9316976dbbbe
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-13 10:32:17 +0100

    [Commands] #1 Refactoriced command

--- commands/purge.js
+++ commands/purge.js
@@ -2,21 +2,17 @@ module.exports = {
   name: 'purge',
   description: 'purge command',
   execute(msg, args) {
-    const messagecount = parseInt(args[1]) || 1;
+    const args = msg.content.split(' ').slice(1); // All arguments behind the command name with the prefix
+    const amount = args.join(' '); // Amount of messages which should be deleted
 
-        var deletedMessages = -1;
+    if (!amount) return msg.reply('You haven\'t given an amount of messages which should be deleted!'); // Checks if the amount parameter is given
+    if (isNaN(amount)) return msg.reply('The amount parameter isn`t a number!'); // Checks if the amount parameter is a number. If not, the command throws an error
 
-        msg.channel.fetchMessages({limit: Math.min(messagecount + 1, 100)}).then(messages => {
-            messages.forEach(m => {
-                if (m.author.id == bot.user.id) {
-                    m.delete().catch(console.error);
-                    deletedMessages++;
-                }
-            });
-        }).then(() => {
-                if (deletedMessages === -1) deletedMessages = 0;
-                msg.channel.send(`:white_check_mark: Purged \`${deletedMessages}\` messages.`)
-                    .then(m => m.delete(2000));
-        }).catch(console.error);
+    if (amount > 100) return msg.reply('You can`t delete more than 100 messages at once!'); // Checks if the amount integer is bigger than 100
+    if (amount < 1) return msg.reply('You have to delete at least 1 message!'); // Checks if the amount integer is smaller than 1
+
+    await msg.channel.messages.fetch({ limit: amount }).then(messages => { // Fetches the messages
+        msg.channel.bulkDelete(messages 
+    )});
   },
 };
