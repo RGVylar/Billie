77a9cb97493dbac744c8da9cb362183345fc09a4
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-13 10:48:09 +0100

    [Commands] #1 Command done

--- commands/purge.js
+++ commands/purge.js
@@ -2,13 +2,12 @@ module.exports = {
   name: 'purge',
   description: 'purge command',
     execute: async (msg, args) => {
-    //const args = msg.content.split(' ').slice(1); // All arguments behind the command name with the prefix
     const amount = args.join(' '); // Amount of messages which should be deleted
 
     if (!amount) return msg.reply('You haven\'t given an amount of messages which should be deleted!'); // Checks if the amount parameter is given
-    if (isNaN(amount)) return msg.reply('The amount parameter isn`t a number!'); // Checks if the amount parameter is a number. If not, the command throws an error
+    if (isNaN(amount)) return msg.reply('The amount parameter isn\'t a number!'); // Checks if the amount parameter is a number. If not, the command throws an error
 
-    if (amount > 100) return msg.reply('You can`t delete more than 100 messages at once!'); // Checks if the amount integer is bigger than 100
+    if (amount > 100) return msg.reply('You can\'t delete more than 100 messages at once!'); // Checks if the amount integer is bigger than 100
     if (amount < 1) return msg.reply('You have to delete at least 1 message!'); // Checks if the amount integer is smaller than 1
 
     await msg.channel.fetchMessages({ limit: amount }).then(messages => { // Fetches the messages
