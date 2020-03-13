ebf9ae92874ccf78a482de5d79cf61c673ed485b
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-13 19:28:46 +0100

    Update mongo.js

--- commands/mongo.js
+++ commands/mongo.js
@@ -1,19 +1,22 @@
 const MongoClient = require('mongodb').MongoClient;
 const config = require("../config.js");
 module.exports = {
-  name: 'mongo',
-  description: 'mongo!',
+  name: 'skiss',
+  description: 'set kiss!',
   execute(msg, args) {
-	const MONGO = config.MONGO;
-  	MongoClient.connect(MONGO, function(err, db) {
-	  if (err) throw err;
-	  var dbo = db.db("billie");
-	  var myobj = { name: "Company Inc", address: "Highway 37" };
-	  dbo.collection("billie").insertOne(myobj, function(err, res) {
-	    if (err) throw err;
-	    msg.channel.send("1 document inserted");
-	    db.close();
-	  });
-	});
+  	if (!args || args == "") {msg.channel.send("I need an url");}
+  	else{
+		const MONGO = config.MONGO;
+	  	MongoClient.connect(MONGO, function(err, db) {
+		  if (err) throw err;
+		  var dbo = db.db("billie");
+		  var myobj = { url:args };
+		  dbo.collection("kiss").insertOne(myobj, function(err, res) {
+		    if (err) throw err;
+		    msg.channel.send("1 Kiss inserted");
+		    db.close();
+		  });
+		});
+	}
   },
 };
