b5dcdc250f41f03ff1f88beded9a56d412261a28
Az <40351751+Mystos@users.noreply.github.com>
2020-03-14 11:05:04 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -5,8 +5,8 @@ module.exports = {
   execute: async (msg, args) => {
       const ytdl = require('ytdl-core');
 
-      if (msg.member.voice.channel) {
-          const connection = await msg.member.voice.channel.join();
+      if (msg.member.voiceChannel) {
+          const connection = await msg.member.voiceChannel.join();
           console.log("joined channel");
           // the bot is connected
           const ytdl = require('ytdl-core');
