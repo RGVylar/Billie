d06d60b3f2777c2ebb506ea05f3f9554d9378d88
Az <40351751+Mystos@users.noreply.github.com>
2020-03-14 10:38:19 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -3,15 +3,15 @@ module.exports = {
   description: 'play!',
   execute: async (msg, args) => {
     const ytdl = require('ytdl-core');
-      const connection = await msg.member.voiceChannel.join().then(connection => {
-            console.log("joined channel");
-            const stream = ytdl('https://youtu.be/nGfS3y3wnfc', { filter : 'audioonly' });
-            const dispatcher = connection.play(stream);
+    const connection = await msg.member.voiceChannel.join().then(connection => {
+        console.log("joined channel");
+        const stream = ytdl('https://youtu.be/nGfS3y3wnfc', { filter : 'audioonly' });
+        const dispatcher = this.play(stream);
 
-            dispatcher.on("end", end => {
-                console.log("left channel");
-                voiceChannel.leave();
-            });
-        }).catch(err => console.log(err));
+        dispatcher.on("end", end => {
+            console.log("left channel");
+            voiceChannel.leave();
+        });
+    }).catch(err => console.log(err));
   },
 };
