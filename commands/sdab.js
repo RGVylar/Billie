49470cef3206b02559cade59546200ddce06c328
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-14 14:59:24 +0100

    big update

---
+++ commands/sdab.js
@@ -0,0 +1,25 @@
+const MongoClient = require('mongodb').MongoClient;
+const config = require("../config.js");
+module.exports = {
+  name: 'sdab',
+  description: 'set dab!',
+  execute(msg, args) {
+	const MONGO = config.MONGO;
+    if(msg.member.roles.find(r => r.name === "tester")){
+	  	if (!args || args == "") {msg.channel.send("I need an url");}	
+	  	else{
+		  	MongoClient.connect(MONGO, function(err, db) {
+			  if (err) throw err;
+			  var dbo = db.db("billie");
+			  var myobj = { url:args };
+			  dbo.collection("dab").insertOne(myobj, function(err, res) {
+			    if (err) throw err;
+			    msg.channel.send("1 dab inserted");
+			    db.close();
+			  });
+			});
+		}
+	}else{msg.channel.send("You dont have permission");}
+	msg.delete();
+  },
+};
