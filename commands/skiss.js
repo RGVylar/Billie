6e21d57fdb2cc356071e4a14a9b3741e9b7f6b18
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-14 01:35:45 +0100

    Update skiss.js

--- commands/skiss.js
+++ commands/skiss.js
@@ -4,22 +4,35 @@ module.exports = {
   name: 'skiss',
   description: 'set kiss!',
   execute(msg, args) {
+	const MONGO = config.MONGO;
     if(msg.member.roles.find(r => r.name === "tester")){
 	  	if (!args || args == "") {msg.channel.send("I need an url");}
+	  	else if (args.includes('-l')) {
+	  		args.splice(args.indexOf('-l'), 1);
+	  		MongoClient.connect(MONGO, function(err, db) {
+			  if (err) throw err;
+			  var dbo = db.db("billie");
+			  var myobj = { url:args };
+			  dbo.collection("kiss").insertOne(myobj, function(err, res) {
+			    if (err) throw err;
+			    msg.channel.send("1 lewdss inserted");
+			    db.close();
+			  });
+			});
+		}	
 	  	else{
-			const MONGO = config.MONGO;
 		  	MongoClient.connect(MONGO, function(err, db) {
 			  if (err) throw err;
 			  var dbo = db.db("billie");
 			  var myobj = { url:args };
 			  dbo.collection("kiss").insertOne(myobj, function(err, res) {
 			    if (err) throw err;
-			    msg.channel.send("1 Kiss inserted");
+			    msg.channel.send("1 kiss inserted");
 			    db.close();
 			  });
 			});
 		}
-	}
+	}else{msg.channel.send("You dont have permission");}
 	msg.delete();
   },
 };
