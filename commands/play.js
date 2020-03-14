ac9e97071913f6acbd3f55f2a0bb360096c27f0b
Az <40351751+Mystos@users.noreply.github.com>
2020-03-14 10:57:13 +0100

    Update play.js

--- commands/play.js
+++ commands/play.js
@@ -1,17 +1,26 @@
+const Discord = require('discord.js');
 module.exports = {
   name: 'play',
   description: 'play!',
   execute: async (msg, args) => {
-    const ytdl = require('ytdl-core');
-    const connection = await msg.member.voiceChannel.join().then(connection => {
-        console.log("joined channel");
-        const stream = ytdl('https://youtu.be/nGfS3y3wnfc', { filter : 'audioonly' });
-        const dispatcher = this.play(stream);
+      const ytdl = require('ytdl-core');
 
-        dispatcher.on("end", end => {
-            console.log("left channel");
-            voiceChannel.leave();
-        });
-    }).catch(err => console.log(err));
+      if (msg.member.voice.channel) {
+          const connection = await msg.member.voice.channel.join();
+          console.log("joined channel");
+          // the bot is connected
+          const ytdl = require('ytdl-core');
+          const dispatcher = connection.play(ytdl('https://www.youtube.com/watch?v=ZlAU_w7-Xp8', { filter: 'audioonly' }));
+
+
+          dispatcher.on("end", end => {
+              console.log("left channel");
+              voiceChannel.leave();
+
+          }).catch(err => console.log(err));
+      } else {
+          msg.reply('You need to join a voice channel first!');
+      }
+      
   },
 };
