2e2422efcbb7537adcbc617e03270cc439a20409
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-15 17:17:52 +0100

    help update

--- commands/help.js
+++ commands/help.js
@@ -16,150 +16,83 @@ module.exports = {
           if (!msg.channel.nsfw) {      
             const embed1 = new Discord.RichEmbed()
             .setColor('#99cc00')
-            .setTitle('Commands')
-            .setDescription('The list of the commands')
-            .addField('sa', 'Command', true)
-            .addField('cry', 'Command', true)
-            .addField('cum', 'Command', true)
-            .addField('dab', 'Command', true)
-            .addField('say', 'Command', true)
-            .addField('hug', 'Command', true)
-            .addField('rps', 'Command', true)
-            .addField('tea', 'Command', true)
-            .addField('join', 'Command', true)
-            .addField('coin', 'Command', true)
-            .addField('stop', 'Command', true)
-            .addField('play', 'Command', true)
-            .addField('help', 'Command', true)
-            .addField('ping', 'Command', true)
-            .addField('turn', 'Command', true)
-            .addField('suck', 'Command', true)
-            .addField('dice', 'Command', true)
-            .addField('date', 'Command', true)
-            .addField('kick', 'Command', true)
-            .addField('kiss', 'Command', true)
-            .addField('nsfw', 'Command', true)
-            .addField('loli', 'Command', true)
-            .addField('fuck', 'Command', true)
-            .addField('scum', 'Command', true)
+            .setTitle('Admin Commands')
+            .setDescription('The list of the Admin commands')
             .addField('scry', 'Command', true)
+            .addField('sdab', 'Command', true)
+            .addField('shug', 'Command', true)
+            .addField('stea', 'Command', true)
+            .addField('punch', 'Command', true)
+            .addField('purge', 'Command', true)
+            .addField('skiss', 'Command', true)
+            .addField('skick', 'Command', true)
+            .addField('sdance', 'Command', true)
+            .addField('prefix', 'Command', true)
+            .addField('scursed', 'Command', true)
+            .addField('newcommand', 'Command', true)
             .setFooter('The prefix right now is `'+PREFIX+'`', 'https://cdn.discordapp.com/emojis/679413153423163392.gif?v=1');
-            const embed2 = new Discord.RichEmbed()
+            msg.channel.send(admin);
+
+          }
+
+          //Music Commands
+          const music = new Discord.RichEmbed()
+          .setColor('#FF0000')
+          .setTitle('Music Commands')
+          .setDescription('The list of Music commands')
+          .addField('join', 'Command', true)
+          .addField('stop', 'Command', true)
+          .addField('play', 'Command', true)
+          .setFooter('The prefix right now is `'+PREFIX+'`', 'https://cdn.discordapp.com/emojis/679413153423163392.gif?v=1');
+          msg.channel.send(music);
+
+          if (!msg.channel.nsfw) {  
+
+            //NSFW Commands
+            const nsfw = new Discord.RichEmbed()
             .setColor('#FF0000')
-            .setTitle('Commands 2')
-            .setDescription('The list of the commands')
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
+            .setTitle('Commands NSFW')
+            .setDescription('The list of NSFW commands')
+            .addField('NSFW Commands in this channel', 'Please, ask for the commands on a NSFW channel', true)
             .setFooter('The prefix right now is `'+PREFIX+'`', 'https://cdn.discordapp.com/emojis/679413153423163392.gif?v=1');
-            const embed3 = new Discord.RichEmbed()
-            .setColor('#FF0000')
-            .setTitle('Commands 3')
-            .setDescription('The list of the commands')
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .setFooter('The prefix right now is `'+PREFIX+'`', 'https://cdn.discordapp.com/emojis/679413153423163392.gif?v=1');
-            msg.channel.send(embed1);
-            msg.channel.send(embed2);
-            msg.channel.send(embed3);
+            msg.channel.send(nsfw);
+
+            if(msg.member.roles.find(r => r.name === "tester")){
+
+              //ADMIN NSFW Commands
+              const adminNsfw = new Discord.RichEmbed()
+              .setColor('#FF0000')
+              .setTitle('Admin nsfw Commands')
+              .setDescription('The list of NSFW Admin commands')
+              .addField('scum', 'Command', true)
+              .addField('ssuck', 'Command', true)
+              .addField('sfuck', 'Command', true)
+              .addField('scunni', 'Command', true)
+              .addField('sundress', 'Command', true)
+              .addField('smasturbate', 'Command', true)
+              .setFooter('The prefix right now is `'+PREFIX+'`', 'https://cdn.discordapp.com/emojis/679413153423163392.gif?v=1');
+              msg.channel.send(adminNsfw);   
+
+            }
           }
           else {
-            const embed1 = new Discord.RichEmbed()
+
+            //NSFW Commands
+            const nsfw = new Discord.RichEmbed()
             .setColor('#FF0000')
-            .setTitle('Commands')
-            .setDescription('The list of the commands')
-            .addField('sa', 'Command', true)
-            .addField('cry', 'Command', true)
+            .setTitle('Commands NSFW')
+            .setDescription('The list of NSFW commands')
             .addField('cum', 'Command', true)
-            .addField('dab', 'Command', true)
-            .addField('say', 'Command', true)
-            .addField('hug', 'Command', true)
-            .addField('rps', 'Command', true)
-            .addField('tea', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
+            .addField('suck', 'Command', true)
+            .addField('nsfw', 'Command', true)
+            .addField('fuck', 'Command', true)
+            .addField('cunni', 'Command', true)
+            .addField('undress', 'Command', true)
+            .addField('urbansfw', 'Command', true)
+            .addField('masturbate', 'Command', true)
             .setFooter('The prefix right now is `'+PREFIX+'`', 'https://cdn.discordapp.com/emojis/679413153423163392.gif?v=1');
-            const embed2 = new Discord.RichEmbed()
-            .setColor('#FF0000')
-            .setTitle('Commands 2')
-            .setDescription('The list of the commands')
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .setFooter('The prefix right now is `'+PREFIX+'`', 'https://cdn.discordapp.com/emojis/679413153423163392.gif?v=1');
-            const embed3 = new Discord.RichEmbed()
-            .setColor('#FF0000')
-            .setTitle('Commands 2')
-            .setDescription('The list of the commands')
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .addField('Command', 'Command', true)
-            .setFooter('The prefix right now is `'+PREFIX+'`', 'https://cdn.discordapp.com/emojis/679413153423163392.gif?v=1');
-            msg.channel.send(embed1);
-            msg.channel.send(embed2);
-            msg.channel.send(embed3);
+            msg.channel.send(nsfw);   
+
           }
         }); 
       db.close();
