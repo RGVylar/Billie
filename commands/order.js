db24585f1feb8ab430ac9ec7e1f9829391340342
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-15 10:22:06 +0100

    Update order.js

--- commands/order.js
+++ commands/order.js
@@ -4,12 +4,12 @@ module.exports = {
     description: 'show monogatari order',
     execute(msg, args) {
       const exampleEmbed = new Discord.RichEmbed()
-      .setColor('#0099ff')
+      .setColor('#ffff00')
       .setTitle('Monogatari series watch order')
       .setURL('https://media.discordapp.net/attachments/652432414135681060/662034140505571378/6gqy1AQaz0AXwlkBaVPMP-ST8fwleVWMLFXAcWkBHOM.png?width=617&height=904')
       //.setAuthor('Some name', 'https://cdn.discordapp.com/emojis/679413153423163392.gif?v=1', 'https://discord.js.org')
       .setDescription('Suggested watch order')
-      .setThumbnail('https://cdn.discordapp.com/emojis/679413153423163392.gif?v=1')
+      .setThumbnail('https://cdn.discord.me/server/99cc19f10df5da7c09263ff47437dd8a700275be98bf88ae8c0c360a4ea0176d/icon_c47e9a6c64d7043618d92c7fb6125d1268f6a40497490ea377d9bba4bef12a27.jpg')
       .addField('Bakemonogatari', '15 Episodes', true)
       .addField('Kisumonogatari', '3 Movies', true)
       .addField('Nisemonogatari', '11 Episodes', true)
@@ -22,9 +22,9 @@ module.exports = {
       .addField('Koyomimonogatari', '12 Hald-Episodes', true)
       .addField('Owarimonogatari\nSecond Season', '7 Episodes', true)
       .addField('Zoku Owarimonogatari', '6 Episodes', true)
-      .setImage('https://cdn.discordapp.com/emojis/679413153423163392.gif?v=1')
-      .setTimestamp()
-      .setFooter('Some footer text here', 'https://cdn.discordapp.com/emojis/679413153423163392.gif?v=1');
+      //.setImage('https://cdn.discordapp.com/emojis/679413153423163392.gif?v=1')
+      //.setTimestamp()
+      .setFooter('Made by RGVylar', 'https://cdn.discordapp.com/emojis/675047947246764042.png?v=1');
 
       msg.channel.send(exampleEmbed);
     },
