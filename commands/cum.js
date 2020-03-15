0d3651c506f053827162c2c0c1b9d2b82ad49f15
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-15 20:44:01 +0100

    Update cum.js

--- commands/cum.js
+++ commands/cum.js
@@ -2,24 +2,24 @@ const Discord = require('discord.js');
 const MongoClient = require('mongodb').MongoClient;
 const config = require("../config.js");
 module.exports = {
-  name: 'cum',
-  description: 'cum things!',
-  execute(msg, args) {
-	const MONGO = config.MONGO;
-	if (!msg.channel.nsfw) {
-        const nsfwWrongChannelWarn = new Discord.RichEmbed()
+  	name: 'cum',
+  	description: 'cum things!',
+  	execute(msg, args) {
+		const MONGO = config.MONGO;
+		if (!msg.channel.nsfw) {
+        	const nsfwWrongChannelWarn = new Discord.RichEmbed()
             .setColor('#FF0000')
             .setTitle('You lewd !')
             .setDescription('You need to be in a nsfw channel for that Baa~ Baka')
             .setImage("https://media1.tenor.com/images/8674cfb928b1055dd6b8227e7e61060b/tenor.gif?itemid=7979947");
 
-        msg.channel.send(nsfwWrongChannelWarn);
-    } else {
-	  	if (!msg.mentions.users.size) {
-			msg.channel.send("Find someone :(");
-		}	
-		else{
-				console.log(msg.mentinos.users);
+        	msg.channel.send(nsfwWrongChannelWarn);
+    	}
+    	else {
+	  		if (!msg.mentions.users.size) {
+				msg.channel.send("Find someone :(");
+			}	
+			else {
 				MongoClient.connect(MONGO, function(err, db) {
 			  		if (err) throw err;
 			  		var dbo = db.db("billie");
@@ -28,7 +28,7 @@ module.exports = {
 						const userlist = msg.mentions.users.map(user => {
 							const usera = msg.member.user.tag;
 							const userb = user.tag;
-							if(usera==userb) msg.channel.send("Find someone :("); return;
+							if(usera==userb) msg.channel.send("Find someone else :("); return;
 							const a = usera.indexOf("#");
 							const b = userb.indexOf("#");
 							const  resa = usera.substring(0, a);
