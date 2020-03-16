ad09b5e0e93a68bd6d2853b7a2cf13d3cc1e2cfa
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-16 09:56:55 +0100

    Update hug.js

--- commands/hug.js
+++ commands/hug.js
@@ -40,18 +40,22 @@ module.exports = {
 			  		var dbo = db.db("billie");
 					dbo.collection("hug").find({}).toArray(function(err, result) {
 		    			if (err) throw err;
-						const a = usera.indexOf("#");
-						const b = userb.indexOf("#");
-						const  resa = usera.substring(0, a);
-						const  resb = userb.substring(0, b);
-					    var randomIndex = Math.floor(Math.random() * result.length); 
-					    var gif = result[randomIndex].url;
-					    const exampleEmbed = new Discord.RichEmbed()
-						.setColor('#0099ff')
-						.setTitle(`${resa} hugs ${resb}`)
-						.setImage(gif[0]);
+					  	const userlist = msg.mentions.users.map(user => {
+						  	const usera = msg.member.user.tag;
+							const userb = user.tag;
+							const a = usera.indexOf("#");
+							const b = userb.indexOf("#");
+							const  resa = usera.substring(0, a);
+							const  resb = userb.substring(0, b);
+						    var randomIndex = Math.floor(Math.random() * result.length); 
+						    var gif = result[randomIndex].url;
+						    const exampleEmbed = new Discord.RichEmbed()
+							.setColor('#0099ff')
+							.setTitle(`${resa} hugs ${resb}`)
+							.setImage(gif[0]);
 
-						return msg.channel.send(exampleEmbed);
+							return msg.channel.send(exampleEmbed);
+						}
 					});
 				}
 			}
