ed28a16e09aa584c561d0a42b863b50d263a1103
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-16 15:12:13 +0100

    Update noticeme.js

--- commands/noticeme.js
+++ commands/noticeme.js
@@ -10,7 +10,7 @@ module.exports = {
 			if (err) throw err;
 			var dbo = db.db("billie");
 			var myquery = { user: id };
-			dbo.collection("whitelist").deleteOne(myquery, function(err, obj) {
+			dbo.collection("whitelist").deleteMany(myquery, function(err, obj) {
 			    if (err) throw err;
 			    msg.channel.send("Billie desu");
 			    db.close();
