67d29a80fbe4d808603abfd344e4495dc3bed0d0
Az <40351751+Mystos@users.noreply.github.com>
2020-03-16 18:23:38 +0100

    Update nsfw.js

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -104,7 +104,19 @@ module.exports = {
             await fetch(urlSearch)
                 .then(response => response.json())
                 .then(data => {
-                    console.log(urlSearch);
+                    console.log(urlSearch, {
+                        method: 'GET', // *GET, POST, PUT, DELETE, etc.
+                        mode: 'cors', // no-cors, *cors, same-origin
+                        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+                        credentials: 'same-origin', // include, *same-origin, omit
+                        headers: {
+                            'Content-Type': 'application/json'
+                            // 'Content-Type': 'application/x-www-form-urlencoded',
+                        },
+                        redirect: 'follow', // manual, *follow, error
+                        referrerPolicy: 'no-referrer', // no-referrer, *client
+                        body: JSON.stringify(data) // body data type must match "Content-Type" header
+                    });
                     console.log('fetched');
                     console.table(data);
                     if (data.length == 0) {
