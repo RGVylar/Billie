27d12d884fcdb4d900f934b9866909d31f34db58
Az <40351751+Mystos@users.noreply.github.com>
2020-03-16 14:29:42 +0100

    Update nsfw.js

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -92,8 +92,7 @@ module.exports = {
                 urlPost = 'https://gelbooru.com/index.php?page=post&s=view&id='
 
             } else if (sourceWebsite == 'c'){
-                //urlSearch = "https://capi-v2.sankakucomplex.com/posts?lang=english&page=1&limit=100&tags=" + urlTag;
-                urlSearch = "https://gelbooru.com/index.php?page=dapi&s=post&q=index&json=1&tag=" + urlTag;
+                urlSearch = "https://capi-v2.sankakucomplex.com/posts?lang=english&page=1&limit=100&tags=" + urlTag;
                 urlPost = 'https://chan.sankakucomplex.com/post/show/'
             }
 
