4c5d45c5b8c7724221695e1f3e8829a92f50e4ff
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-16 14:34:50 +0100

    The filter

--- commands/cunni.js
+++ commands/cunni.js
@@ -2,33 +2,52 @@ const Discord = require('discord.js');
 const MongoClient = require('mongodb').MongoClient;
 const config = require("../config.js");
 module.exports = {
-  name: 'cunni',
-  description: 'cunni things!',
-  execute(msg, args) {
-	const MONGO = config.MONGO;
-	if (!msg.channel.nsfw) {
-        const nsfwWrongChannelWarn = new Discord.RichEmbed()
+  	name: 'cunni',
+  	description: 'love things!',
+  	execute(msg, args) {
+		var whitelisted = false;
+		const MONGO = config.MONGO;
+		if (!msg.channel.nsfw) {
+        	const nsfwWrongChannelWarn = new Discord.RichEmbed()
             .setColor('#FF0000')
             .setTitle('You lewd !')
             .setDescription('You need to be in a nsfw channel for that Baa~ Baka')
             .setImage("https://media1.tenor.com/images/8674cfb928b1055dd6b8227e7e61060b/tenor.gif?itemid=7979947");
 
-        msg.channel.send(nsfwWrongChannelWarn);
-    } else {
-	  	if (!msg.mentions.users.size) {
-			msg.channel.send("Find someone :(");
-		}	
-		else{
+        	msg.channel.send(nsfwWrongChannelWarn);
+    	}
+    	else {
+	  		if (!msg.mentions.users.size) {
+				msg.channel.send("Find someone :(");
+			}
+			const userlist = msg.mentions.users.map(user => {
+				const usera = msg.member.user.tag;
+				const userb = user.tag;
+				const id = user.id;
+				var whitelisted = MongoClient.connect(MONGO, function(err, db) {
+				  	if (err) throw err;
+				  	var dbo = db.db("billie");
+				  	var query = { user: id };
+				  	dbo.collection("whitelist").find(query).toArray(function(err, result) {
+				    	if (err) throw err;
+				    	if(typeof result[0] !== 'undefined'){
+					    	if(result[0].user==id){
+					    		whitelisted=true;
+					    	}	
+				    	}
+				    	db.close();
+				  	});
+				});	
 				MongoClient.connect(MONGO, function(err, db) {
 			  		if (err) throw err;
 			  		var dbo = db.db("billie");
 			  		dbo.collection("cunni").find({}).toArray(function(err, result) {
 			    		if (err) throw err;
-						const userlist = msg.mentions.users.map(user => {
-							const usera = msg.member.user.tag;
-							const userb = user.tag;
 							if(usera==userb){
 								return msg.channel.send("Find someone else :(");
+							}
+							else if(whitelisted){
+								return msg.channel.send("Who? Someone who doesn't want to be bothered?");	
 							}
 							else {
 								const a = usera.indexOf("#");
@@ -39,7 +58,7 @@ module.exports = {
 							    var gif = result[randomIndex].url;
 							    const exampleEmbed = new Discord.RichEmbed()
 								.setColor('#ffc0cb')
-								.setTitle(`${resa} makes sweet love to  ${resb}s pussy`)
+								.setTitle(`${resa} makes sweet love to ${resb}s pussy`)
 								.setImage(gif[0]);
 			  
 								return msg.channel.send(exampleEmbed);
@@ -47,8 +66,7 @@ module.exports = {
 						}); 
 						db.close();
 					});
-				});
-			}
+			});
 	  	}
 	},
 };
--- commands/kiss.js
+++ commands/kiss.js
@@ -2,30 +2,74 @@ const Discord = require('discord.js');
 const MongoClient = require('mongodb').MongoClient;
 const config = require("../config.js");
 module.exports = {
-	name: 'kiss',
-	description: 'kiss!',
-	execute(msg, args) {
+  	name: 'kiss',
+  	description: 'kiss!',
+  	execute(msg, args) {
+		var whitelisted = false;
 		const MONGO = config.MONGO;
 	  	if (!msg.mentions.users.size) {
-			const exampleEmbed = new Discord.RichEmbed()
-			.setColor('#0099ff')
-			.setTitle(`Is that even posible?`)
-			.setImage("https://media1.tenor.com/images/a25726b66284a8932a17e63360819f3c/tenor.gif?itemid=12015413");
-	  
-			return msg.channel.send(exampleEmbed);
+			msg.channel.send("Find someone :(");
 		}
-		if (args.includes('-l')) {
-	  		args.splice(args.indexOf('-l'), 1);
-			MongoClient.connect(MONGO, function(err, db) {
-				if (err) throw err;
-				var dbo = db.db("billie");
-				dbo.collection("lewdss").find({}).toArray(function(err, result) {
-				    if (err) throw err;
-					const userlist = msg.mentions.users.map(user => {
-						const usera = msg.member.user.tag;
-						const userb = user.tag;
+		const userlist = msg.mentions.users.map(user => {
+			const usera = msg.member.user.tag;
+			const userb = user.tag;
+			const id = user.id;
+			var whitelisted = MongoClient.connect(MONGO, function(err, db) {
+			  	if (err) throw err;
+			  	var dbo = db.db("billie");
+			  	var query = { user: id };
+			  	dbo.collection("whitelist").find(query).toArray(function(err, result) {
+			    	if (err) throw err;
+			    	if(typeof result[0] !== 'undefined'){
+				    	if(result[0].user==id){
+				    		whitelisted=true;
+				    	}	
+			    	}
+			    	db.close();
+			  	});
+			});	
+			if (args.includes('-l')) {
+  				args.splice(args.indexOf('-l'), 1);
+				MongoClient.connect(MONGO, function(err, db) {
+			  		if (err) throw err;
+			  		var dbo = db.db("billie");
+			  		dbo.collection("lewdss").find({}).toArray(function(err, result) {
+		    			if (err) throw err;
 						if(usera==userb){
 							return msg.channel.send("Find someone else :(");
+					}
+					else if(whitelisted){
+						return msg.channel.send("Who? Someone who doesn't want to be bothered?");	
+					}
+					else {
+						const a = usera.indexOf("#");
+						const b = userb.indexOf("#");
+						const  resa = usera.substring(0, a);
+						const  resb = userb.substring(0, b);
+					    var randomIndex = Math.floor(Math.random() * result.length); 
+					    var gif = result[randomIndex].url;
+					    const exampleEmbed = new Discord.RichEmbed()
+						.setColor('#ffc0cb')
+						.setTitle(`${resa} kisses ${resb}`)
+						.setImage(gif[0]);
+  
+						return msg.channel.send(exampleEmbed);
+						}
+					}); 
+					db.close();
+				});
+			}	
+			else{
+				MongoClient.connect(MONGO, function(err, db) {
+		  			if (err) throw err;
+		  			var dbo = db.db("billie");
+		  			dbo.collection("kiss").find({}).toArray(function(err, result) {
+			    		if (err) throw err;
+						if(usera==userb){
+							return msg.channel.send("Find someone else :(");
+						}
+						else if(whitelisted){
+							return msg.channel.send("Who? Someone who doesn't want to be bothered?");	
 						}
 						else {
 							const a = usera.indexOf("#");
@@ -35,45 +79,18 @@ module.exports = {
 						    var randomIndex = Math.floor(Math.random() * result.length); 
 						    var gif = result[randomIndex].url;
 						    const exampleEmbed = new Discord.RichEmbed()
-							.setColor('#0099ff')
+							.setColor('#ffc0cb')
 							.setTitle(`${resa} kisses ${resb}`)
 							.setImage(gif[0]);
+		  
 							return msg.channel.send(exampleEmbed);
 						}
 					}); 
 					db.close();
 				});
-			});
-		}
-		else{
-			MongoClient.connect(MONGO, function(err, db) {
-				if (err) throw err;
-				var dbo = db.db("billie");
-				dbo.collection("kiss").find({}).toArray(function(err, result) {
-				    if (err) throw err;
-					const userlist = msg.mentions.users.map(user => {
-						const usera = msg.member.user.tag;
-						const userb = user.tag;
-						if(usera==userb){
-							return msg.channel.send("Find someone else :(");
-						}
-						else {
-							const a = usera.indexOf("#");
-							const b = userb.indexOf("#");
-							const  resa = usera.substring(0, a);
-							const  resb = userb.substring(0, b);
-						    var randomIndex = Math.floor(Math.random() * result.length); 
-						    var gif = result[randomIndex].url;
-						    const exampleEmbed = new Discord.RichEmbed()
-							.setColor('#0099ff')
-							.setTitle(`${resa} kisses ${resb}`)
-							.setImage(gif[0]);
-							return msg.channel.send(exampleEmbed);
-						}
-					}); 
-					db.close();
-				});
-			});
-		}
-  	},
+
+			}
+		});
+	  	
+	},
 };
