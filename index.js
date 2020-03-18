ffb116ce833ba86ecbfe86ff970d186c30e02ec3
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-18 17:02:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -45,9 +45,11 @@ MongoClient.connect(MONGO, function(err, db) {
               console.log("Config inserted");
             }
           });
+
+      db.close();
     }
   });
-  dbo.createCollection("whitelist", function(err, res) {
+  /*dbo.createCollection("whitelist", function(err, res) {
     if (err) {
       console.log("Whitelist exist");
     }
@@ -55,7 +57,7 @@ MongoClient.connect(MONGO, function(err, db) {
       console.log("Whitelist created");
     }
   });
-  db.close();
+  db.close();*/
 });
 console.log("Bot checked db");
 MongoClient.connect(MONGO, function(err, db) {
