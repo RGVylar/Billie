1631c8127e58cebd5a1c52c644d79803edc0b703
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-19 16:35:12 +0100

    hard part

--- commands/sfuck.js
+++ commands/sfuck.js
@@ -5,51 +5,75 @@ const DEV2 = config.DEV2;
 const DEV5 = config.DEV5;
 const DB = config.DB;
 module.exports = {
-  name: 'sfuck',
-  description: 'set fuck!',
-  execute(msg, args) {
-    if(msg.member.id==DEV||msg.member.id==DEV2||msg.member.id==DEV5){
-		const MONGO = config.MONGO;
-	  	if (!args || args == "") {msg.channel.send("I need an url");}
-	  	else if (args.includes('-s')) {
-	  		args.splice(args.indexOf('-s'), 1);
-	  		MongoClient.connect(MONGO, function(err, db) {
-			  if (err) throw err;
-			  var dbo = db.db(DB);
-			  var myobj = { url:args };
-			  dbo.collection("strapon").insertOne(myobj, function(err, res) {
-			    if (err) throw err;
-			    msg.channel.send("1 strapon lewd inserted");
-			    db.close();
-			  });
-			});
-	  }
-	  	else if (args.includes('-a')) {
-	  		args.splice(args.indexOf('-a'), 1);
-	  		MongoClient.connect(MONGO, function(err, db) {
-			  if (err) throw err;
-			  var dbo = db.db(DB);
-			  var myobj = { url:args };
-			  dbo.collection("anal").insertOne(myobj, function(err, res) {
-			    if (err) throw err;
-			    msg.channel.send("1 anal lewd inserted");
-			    db.close();
-			  });
-			});
-	  }
-	  	else{
-	  		MongoClient.connect(MONGO, function(err, db) {
-			  if (err) throw err;
-			  var dbo = db.db(DB);
-			  var myobj = { url:args };
-			  dbo.collection("fuck").insertOne(myobj, function(err, res) {
-			    if (err) throw err;
-			    msg.channel.send("1 lewd inserted");
-			    db.close();
-			  });
-			});
-		}
-	}else{msg.channel.send("You dont have permission");}
-	msg.delete();
-  },
+	name: 'sfuck',
+	description: 'set fuck!',
+	execute(msg, args) {
+		if(msg.member.id==DEV||msg.member.id==DEV2||msg.member.id==DEV5){
+			const MONGO = config.MONGO;
+			if (!args || args == "") {msg.channel.send("I need an url");}
+			else if (args.includes('-s')) {
+				args.splice(args.indexOf('-s'), 1);
+				MongoClient.connect(MONGO, function(err, db) {
+					if (err) throw err;
+					var dbo = db.db(DB);
+					var myobj = { url:args };
+					dbo.createCollection("strapon", function(err, res) {
+						if (err) {
+						}
+						if(typeof res !== 'undefined'){
+
+							dbo.collection("strapon").insertOne(myobj, function(err, res) {
+								if (err) throw err;
+								msg.channel.send("1 strapon inserted");
+								db.close();
+							});
+						}
+						db.close();
+					});
+				});
+			}
+			else if (args.includes('-a')) {
+				args.splice(args.indexOf('-a'), 1);
+				MongoClient.connect(MONGO, function(err, db) {
+					if (err) throw err;
+					var dbo = db.db(DB);
+					var myobj = { url:args };
+					dbo.createCollection("anal", function(err, res) {
+						if (err) {
+						}
+						if(typeof res !== 'undefined'){
+
+							dbo.collection("anal").insertOne(myobj, function(err, res) {
+								if (err) throw err;
+								msg.channel.send("1 anal inserted");
+								db.close();
+							});
+						}
+						db.close();
+					});
+				});
+			}
+			else{
+				MongoClient.connect(MONGO, function(err, db) {
+					if (err) throw err;
+					var dbo = db.db(DB);
+					var myobj = { url:args };
+					dbo.createCollection("fuck", function(err, res) {
+						if (err) {
+						}
+						if(typeof res !== 'undefined'){
+
+							dbo.collection("fuck").insertOne(myobj, function(err, res) {
+								if (err) throw err;
+								msg.channel.send("1 fuck inserted");
+								db.close();
+							});
+						}
+						db.close();
+					});
+				});
+			}
+		}else{msg.channel.send("You dont have permission");}
+		msg.delete();
+	},
 };
--- commands/skiss.js
+++ commands/skiss.js
@@ -3,38 +3,54 @@ const config = require("../config.js");
 const DEV = config.DEV;
 const DB = config.DB;
 module.exports = {
-  name: 'skiss',
-  description: 'set kiss!',
-  execute(msg, args) {
-	const MONGO = config.MONGO;
-    if(msg.member.id==DEV){
-	  	if (!args || args == "") {msg.channel.send("I need an url");}
-	  	else if (args.includes('-l')) {
-	  		args.splice(args.indexOf('-l'), 1);
-	  		MongoClient.connect(MONGO, function(err, db) {
-			  if (err) throw err;
-			  var dbo = db.db("billie");
-			  var myobj = { url:args };
-			  dbo.collection("lewdss").insertOne(myobj, function(err, res) {
-			    if (err) throw err;
-			    msg.channel.send("1 lewdss inserted");
-			    db.close();
-			  });
-			});
-		}	
-	  	else{
-		  	MongoClient.connect(MONGO, function(err, db) {
-			  if (err) throw err;
-			  var dbo = db.db(DB);
-			  var myobj = { url:args };
-			  dbo.collection("kiss").insertOne(myobj, function(err, res) {
-			    if (err) throw err;
-			    msg.channel.send("1 kiss inserted");
-			    db.close();
-			  });
-			});
-		}
-	}else{msg.channel.send("You dont have permission");}
-	msg.delete();
-  },
+	name: 'skiss',
+	description: 'set kiss!',
+	execute(msg, args) {
+		const MONGO = config.MONGO;
+		if(msg.member.id==DEV){
+			if (!args || args == "") {msg.channel.send("I need an url");}
+			else if (args.includes('-l')) {
+				args.splice(args.indexOf('-l'), 1);
+				MongoClient.connect(MONGO, function(err, db) {
+					if (err) throw err;
+					var dbo = db.db(DB);
+					var myobj = { url:args };
+					dbo.createCollection("lewdss", function(err, res) {
+						if (err) {
+						}
+						if(typeof res !== 'undefined'){
+
+							dbo.collection("lewdss").insertOne(myobj, function(err, res) {
+								if (err) throw err;
+								msg.channel.send("1 lewdss inserted");
+								db.close();
+							});
+						}
+						db.close();
+					});
+				});
+			}	
+			else{
+				MongoClient.connect(MONGO, function(err, db) {
+					if (err) throw err;
+					var dbo = db.db(DB);
+					var myobj = { url:args };
+					dbo.createCollection("kiss", function(err, res) {
+						if (err) {
+						}
+						if(typeof res !== 'undefined'){
+
+							dbo.collection("kiss").insertOne(myobj, function(err, res) {
+								if (err) throw err;
+								msg.channel.send("1 kiss inserted");
+								db.close();
+							});
+						}
+						db.close();
+					});
+				});
+			}
+		}else{msg.channel.send("You dont have permission");}
+		msg.delete();
+	},
 };
