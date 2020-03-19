6ef1a7f67c3c02b15490f4416fc19d11ecec5b34
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-19 16:59:09 +0100

    Update ignoreme.js

--- commands/ignoreme.js
+++ commands/ignoreme.js
@@ -1,20 +1,35 @@
 const MongoClient = require('mongodb').MongoClient;
 const config = require("../config.js");
+const DB = config.DB;
 module.exports = {
-  	name: 'ignoreme',
-  	description: 'set ignoreme!',
-  	execute(msg, args) {
+	name: 'ignoreme',
+	description: 'set ignoreme!',
+	execute(msg, args) {
 		const MONGO = config.MONGO;
-	  	MongoClient.connect(MONGO, function(err, db) {
-		  	if (err) throw err;
-		  	var dbo = db.db("billie");
-		  	const ignored = msg.member.user.id;
-		  	var myobj = { user:ignored };
-		  	dbo.collection("whitelist").insertOne(myobj, function(err, res) {
-			    if (err) throw err;
-			    msg.channel.send("Do you even exist?");
-			    db.close();
-		  	});
+		const ignored = msg.member.user.id;
+		var query = { user:ignored };
+
+		MongoClient.connect(url, function(err, db) {
+			if (err) throw err;
+			var dbo = db.db(DB);
+			dbo.collection("whitelist").find(query).toArray(function(err, result) {
+				if (err) throw err;
+				if(typeof result !== 'undefined'){
+					msg.channel.send("Im already ignoring you");
+				}
+				else{
+					MongoClient.connect(MONGO, function(err, db) {
+						if (err) throw err;
+						var dbo = db.db(DB);
+						dbo.collection("whitelist").insertOne(myobj, function(err, res) {
+							if (err) throw err;
+							msg.channel.send("Do you even exist?");
+							db.close();
+						});
+					});	
+				}
+				db.close();
+			});
 		});
-  	},
+	},
 };
