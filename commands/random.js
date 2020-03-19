87c90d59df7542872dc00e6c14fd399bb3ea5db1
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-20 00:05:27 +0100

    Update random.js

--- commands/random.js
+++ commands/random.js
@@ -13,7 +13,7 @@ module.exports = {
       .setTitle(data.snippet.title)
       .setDescription(data.snippet.description)
       .setURL('https://youtu.be/'+data.id.videoId)
-      .setImage('https://img.youtube.com/vi/'+data.id.videoId+'/0.jpg');
+      .setImage('https://img.youtube.com/vi/'+data.id.videoId+'/0.jpg')
       .setThumbnail('https://cdn.discord.me/server/99cc19f10df5da7c09263ff47437dd8a700275be98bf88ae8c0c360a4ea0176d/icon_c47e9a6c64d7043618d92c7fb6125d1268f6a40497490ea377d9bba4bef12a27.jpg');
       msg.channel.send("https://youtu.be/"+data.id.videoId);
       return msg.channel.send(video);
