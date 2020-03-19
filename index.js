33e4d9679da8401be0f16d1437983d055750837e
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-19 14:24:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -65,6 +65,25 @@ MongoClient.connect(MONGO, function(err, db) {
       }); 
     }
     else {
+      count = 735;
+          cont=count;
+          ++count;
+          newCount=count.toString();
+          PREFIX  = "!";
+          console.log("Config created");
+          var myobj = {  "prefix": [
+          "!"
+          ],
+          "count": "0"};
+          dbo.collection("config").insertOne(myobj, function(err, res) {
+            if (err) {
+              console.log("Error inserting config");
+            }
+            else{    
+              console.log("Config inserted");
+            }
+            db.close();
+          });
     }
     db.close();
   });
