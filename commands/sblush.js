0fe8879cc6cc92c2d5fa974dbc201958771b12bb
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-03-19 15:53:48 +0100

    Update sblush.js

--- commands/sblush.js
+++ commands/sblush.js
@@ -16,24 +16,25 @@ module.exports = {
 					if (err) throw err;
 					var dbo = db.db(DB);
 					var myobj = { url:args };
-			        dbo.createCollection("blush", function(err, res) {
-			          if (err) {
-			          }
-			          if(typeof res !== 'undefined'){
+					dbo.createCollection("blush", function(err, res) {
+						if (err) {
+						}
+						if(typeof res !== 'undefined'){
 
-						dbo.collection("blush").insertOne(myobj, function(err, res) {
-							if (err) throw err;
-							msg.channel.send("1 blush inserted");
-							db.close();
-						});
-					}
-					db.close();
-				});
+							dbo.collection("blush").insertOne(myobj, function(err, res) {
+								if (err) throw err;
+								msg.channel.send("1 blush inserted");
+								db.close();
+							});
+						}
+						db.close();
+					});
+				}
 			}
+			else{
+				msg.channel.send("You dont have permission");
+			}
+			msg.delete();
 		}
-		else{
-			msg.channel.send("You dont have permission");
-		}
-		msg.delete();
 	},
 };
