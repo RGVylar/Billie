e19368d835c34f86e9ca6934eafa5aaddc424e57
Az <40351751+Mystos@users.noreply.github.com>
2020-03-20 13:06:15 +0100

    Update nsfw.js

--- commands/nsfw.js
+++ commands/nsfw.js
@@ -109,6 +109,7 @@ module.exports = {
             },function (error, response, body) {
               if (!error && response.statusCode == 200) {
                 console.log(body);
+                console.log('Proxy work !');
               }
             })
 
