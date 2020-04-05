e8e07d52b029ece6225b4832b90284359a25584c
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-04-05 05:18:27 +0200

    Update sauce.js

--- commands/sauce.js
+++ commands/sauce.js
@@ -2,16 +2,10 @@ const Discord = require('discord.js');
 module.exports = {
   name: 'sauce',
   description: 'sauce!',
-  execute(msg, args) {
-    // Nsfw channel test
+  execute: async (msg, args) =>{
+    var functions = require('../functions/functions.js');
     if (!msg.channel.nsfw) {
-        const nsfwWrongChannelWarn = new Discord.RichEmbed()
-            .setColor('#FF0000')
-            .setTitle('You lewd !')
-            .setDescription('You need to be in a nsfw channel for that Baa~ Baka')
-            .setImage("https://media1.tenor.com/images/8674cfb928b1055dd6b8227e7e61060b/tenor.gif?itemid=7979947");
-
-        msg.channel.send(nsfwWrongChannelWarn);
+        await functions.lewd(msg);
     } 
     else {
         if (!args || args == "") {
