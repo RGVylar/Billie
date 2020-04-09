6d6e3f04cc7bb8f95f7307873f10c1dbd0a4a3d9
RGVylar <35527017+RGVylar@users.noreply.github.com>
2020-04-09 23:02:52 +0200

    Revert "Update config.js"
    This reverts commit 0027ab3f36933355c0bf0e40f07269b967d8b644.

--- config.js
+++ config.js
@@ -1,23 +1,23 @@
-exports.TOKEN = process.env.TOKEN;
+exports.TOKEN = "Njg0MDkwMjI1MTk4MjM1Njk3.Xl1DGQ.fReT_PixyEv2QbsgQRoQHElzKrs";//process.env.TOKEN;
 
-exports.MONGO = process.env.MONGO;
+exports.MONGO = "mongodb://kvoruh:m1l1e5g3n1a7@ds133920.mlab.com:33920/millie";//process.env.MONGO;
 
-exports.DB = process.env.DB;
+exports.DB = "millie";//process.env.DB;
 
-exports.DEV = process.env.DEV;
+exports.DEV = "273081779420921856";//process.env.DEV;
 
-exports.DEV1 = process.env.DEV1;
+exports.DEV1 = "95589920415948800";//process.env.DEV1;
 
-exports.DEV2 = process.env.DEV2;
+exports.DEV2 = "490156163409641473";//process.env.DEV2;
 
-exports.DEV3 = process.env.DEV3;
+exports.DEV3 = "411616384208994305";//process.env.DEV3;
 
-exports.DEV4 = process.env.DEV4;
+exports.DEV4 = "399925589290647552";//process.env.DEV4;
 
-exports.DEV5 = process.env.DEV5;
+exports.DEV5 = "317072026173046794";//process.env.DEV5;
 
-exports.TWITCH = process.env.TWITCH;
+exports.TWITCH = "https://www.twitch.tv/rgvylar";//process.env.TWITCH;
 
-exports.KEY = process.env.KEY;
+exports.KEY = "AIzaSyDuoqSfu6S6BAXRgXtHOPR55Xo1WqTgCzk";//process.env.KEY;
 
 exports.PREFIX = '+';
